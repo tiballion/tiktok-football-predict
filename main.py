3f2b077d131a9ec2caab2327cf24e90a71d752d5
tiballion <timothee.ballion.33@gmail.com>
2022-12-12 02:48:19 +0100

    [EDIT] - added get_match fonction

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from tools.flags import get_flag, get_flag_dict
 from video.create_base_video import save_base_video
 
 
-def main():
+def get_match():
     country1 = input('Enter the first country: ')
     if country1 not in get_flag_dict():
         print('Country not found')
-        return
+        return None
     country2 = input('Enter the second country: ')
     if country2 not in get_flag_dict():
         print('Country not found')
+        return None
+    return Match(country1, country2, get_flag(country1), get_flag(country2), 3)
+
+
+def main():
+    match = get_match()
+    if match is None:
         return
-    match = Match(country1, country2, get_flag(country1), get_flag(country2), 3)
     for m in match.possibilities():
         print(m)
     # save_base_video('test')
