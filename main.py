024a1bcdb56a552210b81615a4ce2c6dc75d09d6
tiballion <timothee.ballion.33@gmail.com>
2022-12-12 03:00:03 +0100

    [EDIT] - refactored get_match()

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from video.create_base_video import save_base_video
 
 
 def get_match():
+    """Return a match object with the two countries and the max score"""
     country1 = input('Enter the first country: ')
     if country1 not in get_flag_dict():
-        print('Country not found')
-        return None
+        print('Country not found'); return
     country2 = input('Enter the second country: ')
+    if country2 == country1:
+        print('The two countries must be different'); return
     if country2 not in get_flag_dict():
-        print('Country not found')
-        return None
-    return Match(country1, country2, get_flag(country1), get_flag(country2), 3)
+        print('Country not found'); return
+    max_score = input('Enter the max score: ')
+    if not max_score.isdigit():
+        print('The max score must be a number'); return
+    return Match(country1, country2, get_flag(country1), get_flag(country2), max_score)
 
 
 def main():
